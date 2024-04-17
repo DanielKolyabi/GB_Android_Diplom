66851337e8d48ea6da2609cf69f527af12ad1d1c
nikitam1337 <nikitam.l@mail.ru>
2024-04-17 22:03:33 +0300

    day 3 in progress

--- App/app/src/main/java/ru/example/gbnotesapp/presentation/fragments/CreateNoteFragment.kt
+++ App/app/src/main/java/ru/example/gbnotesapp/presentation/fragments/CreateNoteFragment.kt
@@ -1,34 +1,65 @@
 package ru.example.gbnotesapp.presentation.fragments
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import ru.example.gbnotesapp.R
-import ru.example.gbnotesapp.presentation.viewmodels.CreateNoteViewModel
+import ru.example.gbnotesapp.databinding.FragmentCreateNoteBinding
+import ru.example.gbnotesapp.presentation.ViewModelFactory
+import ru.example.gbnotesapp.presentation.viewmodels.MainViewModel
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class CreateNoteFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = CreateNoteFragment()
-    }
+    private var _binding: FragmentCreateNoteBinding? = null
+    private val binding
+        get() = _binding!!
 
-    private val viewModel: CreateNoteViewModel by viewModels()
+    @Inject
+    lateinit var createNoteViewModelFactory: ViewModelFactory
+    private val viewModel: MainViewModel by viewModels { createNoteViewModelFactory }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_create_note, container, false)
+        _binding = FragmentCreateNoteBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        clickButtonBack()
+
+
+    }
+
+
+    private fun clickButtonBack() {
+        binding.buttonBack.setOnClickListener {
+            findNavController().navigate(R.id.action_createNoteFragment_to_MainFragment)
+        }
+    }
+
+    private fun clickButtonDone() {
+        binding.buttonBack.setOnClickListener {
+            // TODO сохраняем заметку и остаемся на фрагменте скрывая клавиатуру
+        }
+    }
+
+
+
+
+
+    companion object {
+
     }
 }
--- App/app/src/main/java/ru/example/gbnotesapp/presentation/fragments/ListFoldersFragment.kt
+++ App/app/src/main/java/ru/example/gbnotesapp/presentation/fragments/ListFoldersFragment.kt
@@ -1,34 +1,60 @@
 package ru.example.gbnotesapp.presentation.fragments
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import ru.example.gbnotesapp.R
-import ru.example.gbnotesapp.presentation.viewmodels.ListFoldersViewModel
+import ru.example.gbnotesapp.databinding.FragmentCreateNoteBinding
+import ru.example.gbnotesapp.databinding.FragmentListFoldersBinding
+import ru.example.gbnotesapp.presentation.ViewModelFactory
+import ru.example.gbnotesapp.presentation.viewmodels.MainViewModel
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class ListFoldersFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = ListFoldersFragment()
-    }
+    private var _binding: FragmentListFoldersBinding? = null
+    private val binding
+        get() = _binding!!
 
-    private val viewModel: ListFoldersViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
+    @Inject
+    lateinit var listFoldersViewModelFactory: ViewModelFactory
+    private val viewModel: MainViewModel by viewModels { listFoldersViewModelFactory }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_list_folders, container, false)
+        _binding = FragmentListFoldersBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        clickButtonBack()
+
+
+    }
+
+
+    private fun clickButtonBack() {
+        binding.buttonBack.setOnClickListener {
+            findNavController().navigate(R.id.action_ListFoldersFragment_to_MainFragment)
+        }
+    }
+
+
+
+
+
+
+    companion object {
+
     }
 }
